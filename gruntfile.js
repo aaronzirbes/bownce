a7612097b62e398837a00480974ebad76c566bfd
Michael Cacek <mcacek@bloomhealthco.com>
2014-05-13 18:33:52 -0500

    Initial commit.

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+            src: 'src/<%= pkg.name %>.js',
+            dest: 'build/<%= pkg.name %>.min.js'
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+};
