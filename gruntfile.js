acaa5c705f2a85b5531c2ee0a3c88c5c5dc7aa1d
Michael Cacek <michael.cacek@gmail.com>
2014-05-13 20:11:56 -0500

    Adding grunt functionality and updating file structure.

--- gruntfile.js
+++ gruntfile.js
@@ -3,15 +3,55 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
+        autoprefixer: {
             options: {
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                // browsers: ['last 2 version', 'ie 8', 'ie 9']
+            },
+            all_styles: {
+                /**
+                 * Overrides less output.  This step should always be the last style change.
+                 */
+                src: 'webapp/css/**/*.css'
+            }
         },
-        build: {
-            src: 'src/<%= pkg.name %>.js',
-            dest: 'build/<%= pkg.name %>.min.js'
+        browserify: {
+            all_scripts: {
+                files: {
+                    'webapp/js/common.js': ['bower_components/jquery/dist/jquery.min.js', 'bower_components/angular/angular.min.js'],
+                    'webapp/js/page.js': ['src/js/apps/bownce/**/*.js']
+                }
+            }
+        },
+        eslint: {
+            target: ['src/**/*.js']
+        },
+        less: {
+            all_styles: {
+                options: {},
+                files: {
+                    'webapp/css/common.css': 'src/less/common.less'
+                }
+            }
+        },
+        watch: {
+            everything: {
+                files: ['src/js/**/*.js', 'src/less/**/*.less'],
+                tasks: ['browserify:all_scripts']
+            },
+            all_scripts: {
+                files: ['src/js/**/*.js']
+            },
+            all_styles: {
+                files: ['src/less/**/*.less'],
+                tasks: ['less:all_styles', 'autoprefixer']
+            }
         }
     });
 
     require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['watch:everything']);
+    grunt.registerTask('styles', ['less', 'autoprefixer']);
+    grunt.registerTask('scripts', ['eslint', 'browserify:all_scripts']);
+    grunt.registerTask('test', ['eslint', 'karma']);
 };
---
+++ src/js/bundles/common.js
@@ -0,0 +1,2 @@
+require('../../../bower_components/jquery/dist/jquery.min');
+require('../../../bower_components/angular/angular.min');
